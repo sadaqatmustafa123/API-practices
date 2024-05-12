909ac19b190722508f5ee075e29315867c27c06f
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2024-05-13 03:13:38 +0500

    new screen withot model

---
+++ lib/multi_data_without_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MultiDataWithoutModelScreen extends StatefulWidget {
+  const MultiDataWithoutModelScreen({super.key});
+
+  @override
+  State<MultiDataWithoutModelScreen> createState() => _MultiDataWithoutModelScreenState();
+}
+
+class _MultiDataWithoutModelScreenState extends State<MultiDataWithoutModelScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
